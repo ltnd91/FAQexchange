ae55f55b46ae3acd56894d05ed4b21a595cdfdd0
Long Doan <ltnd91@yahoo.com>
2018-05-07 18:43:59 -0700

    edit distincttest6

--- questions/views.py
+++ questions/views.py
@@ -40,8 +40,8 @@ class QuestionCreateView(LoginRequiredMixin, CreateView):
                 is_following_question.append(ques)
         context['is_following_question'] = is_following_question
         query = self.request.GET.get('q')
-        name_ids = Question.objects.search(query).order_by('-followers', '-updated')
-        qs = Question.objects.filter(id__in=name_ids).distinct()
+        #name_ids = Question.objects.search(query).order_by('-followers', '-updated').distinct()
+        qs = Question.objects.search(query).order_by('-followers', '-updated').distinct()
         context['profiles'] = Profile.objects.filter(followers=self.request.user)
         context['topics'] = Topic.objects.filter(followers=self.request.user)
         if qs.exists():
